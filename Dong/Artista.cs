7107fcb4ec6a1a7e2658a8e154af326ba0f145b5
Fabiano Teodoro <faateodoro@gmail.com>
2020-11-18 16:42:35 -0300

    Adicionando Artista

---
+++ Dong/Artista.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Dong
+{
+    public class Artista
+    {
+        public Artista(string nome, string email, string descricao)
+        {
+            Nome = nome;
+            Email = email;
+            Descricao = descricao;
+        }
+
+        private string _nome;
+        public string Nome
+        {
+            get { return _nome; }
+            set 
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("O nome não pode ser vazio ou nulo.");
+                }
+                _nome = value; 
+            }
+        }
+        
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set 
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("E-mail não pode ser vazio ou nulo.");
+                }
+                if(!Regex.IsMatch(value, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$"))
+                {
+                    throw new ArgumentException("Email está com formato inválido.");
+                }
+                _email = value; 
+            }
+        }
+        
+        private string _descricao;
+        public string Descricao
+        {
+            get { return _descricao; }
+            set 
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Adescrição não pode ser vazia ou nula.");
+                }
+                if (value.Length>500)
+                {
+                    throw new ArgumentException("A descrição contém mais de 500 caracteres.");
+                }
+                _descricao = value; 
+            }
+        }
+        
+        public override string ToString()
+        {
+            return $"Artista: {_nome}, Email: {_email}, Descrição: {_descricao}";
+        }
+
+    }
+}
---
+++ Dong/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Linq;
+
+namespace Dong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Testando criação do artista com valores válidos
+            try
+            {
+                new Artista("Wands do cavaco", "wandsdocavaco@alura.com.br", "O menino manja demais!");
+                Console.WriteLine("Sucesso: Artista criado com os dados válidos.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Falha: {e.Message}");
+            }
+
+            // artista com nome vazio deve lançar erro
+            try
+            {
+                new Artista("", "wandsdocavaco@alura.com.br", "O menino manja demais!");
+                Console.WriteLine("Falha: Artista criado com o nome vazio.");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Sucesso: {e.Message}");
+            }
+
+            // artista com nome nulo deve lançar erro
+            try
+            {
+                new Artista(null, "wandsdocavaco@alura.com.br", "O menino manja demais!");
+                Console.WriteLine("Falha: Artista criado com o nome nulo");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Sucesso: {e.Message}");
+            }
+
+            // artista com email vazio deve lançar erro
+            try
+            {
+                new Artista("Wands do cavaco", "", "O menino manja demais!");
+                Console.WriteLine($"Falha: Artista criado com email vazio.");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Sucesso: {e.Message}");
+
+            }
+
+            // artista com email nulo deve lançar erro
+            try
+            {
+                new Artista("Wands do cavaco", null, "O menino manja demais!");
+                Console.WriteLine($"Falha: Artista criado com email nulo.");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Sucesso: {e.Message}");
+            }
+
+            // artista com email inválido deve lançar erro
+            try
+            {
+                new Artista("Wands do cavaco", "wandsdocavacoalura.com.br", "O menino manja demais!");
+                Console.WriteLine($"Falha: Artista criado com email nulo.");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Sucesso: {e.Message}");
+            }
+
+            // artista com email inválido deve lançar erro
+            try
+            {
+                new Artista("Wands do cavaco", "wandsdocavaco@alura", "O menino manja demais!");
+                Console.WriteLine($"Falha: Artista criado com email nulo.");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Sucesso: {e.Message}");
+            }
+
+            // artista com descrição vazia deve lançar erro
+            try
+            {
+                new Artista("Wands do cavaco", "wandsdocavaco@alura.com.br", "");
+                Console.WriteLine($"Falha: Artista criado com email nulo.");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Sucesso: {e.Message}");
+            }
+
+            // artista com descrição nula deve lançar erro
+            try
+            {
+                new Artista("Wands do cavaco", "wandsdocavaco@alura.com.br", null);
+                Console.WriteLine($"Falha: Artista criado com email nulo.");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Sucesso: {e.Message}");
+            }
+
+            // artista com descrição com mais de 500 caracteres deve lançar erro
+            try
+            {
+                new Artista("Wands do cavaco", "wandsdocavaco@alura.com.br", String.Concat(Enumerable.Repeat("a", 501)));
+                Console.WriteLine($"Falha: Artista criado com email nulo.");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Sucesso: {e.Message}");
+            }
+        }
+    }
+}
